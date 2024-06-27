16a0040c102418e0742815111d56378d0f3a8531
taichomeiso <otaichididit@gmail.com>
2024-06-27 19:57:45 +0900

    ユーザー管理機能単体テスト1

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,4 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
-  
-
 end
