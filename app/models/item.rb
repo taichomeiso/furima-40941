6f1776c4aa8674df1c140156b6d102b417bcc917
taichomeiso <otaichididit@gmail.com>
2024-06-28 16:59:20 +0900

    単体テスト前まで実装

---
+++ app/models/item.rb
@@ -0,0 +1,23 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+ 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_description
+    validates :category_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :condition_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :shipping_cost_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :days_to_ship_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  end
+end
