c75e1979a173eb996f843a34882408e24f408482
taichomeiso <otaichididit@gmail.com>
2024-06-28 10:16:11 +0900

    ユーザー管理機能修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
     validates :nickname
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶヴー]+\z/, message: 'is invalid. Input full-width katakana characters' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶヴー]+\z/, message: 'is invalid. Input full-width katakana characters' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヴー]+\z/u, message: 'is invalid. Input full-width katakana characters' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヴー]+\z/u, message: 'is invalid. Input full-width katakana characters' }
     validates :birthday
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -56,6 +56,21 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
+      it '英字のみのパスワードでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+      end
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = '111111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+      end
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'ああああああ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+      end
       it 'last_nameが空だと登録できない' do
         @user.last_name = ''
         @user.valid?
