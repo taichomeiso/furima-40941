f4bec60f451638176fb9f47e90741b4cbcdce701
taichomeiso <otaichididit@gmail.com>
2024-07-01 14:24:37 +0900

    Revert "Merge branch 'main' into 商品一覧機能の実装"
    This reverts commit f07436bf3b48b542dcc9b86584326e145234ad1c, reversing
    changes made to 8448e8b6fabe25aadc28d5a44c5df909a0728a18.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_root_path, only: [:edit]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -20,30 +18,7 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-    if @item.update(item_params)
-      redirect_to item_path(@item)
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @item.destroy
-    redirect_to root_path
-  end
-
   private
-
-  def set_item
-    @item = Item.find(params[:id])
-  end
 
   def item_params
     params.require(:item).permit(
@@ -57,9 +32,5 @@ class ItemsController < ApplicationController
       :price,
       :image
     ).merge(user_id: current_user.id)
-  end
-
-  def move_to_root_path
-    redirect_to root_path unless current_user == @item.user
   end
 end
